130b5f0b50d881802509440a7c430b9ac397a1ef
jsb-szy <szytwo@qq.com>
2024-12-26 08:04:38 +0800

    优化

---
+++ go-api-conda1.bat
@@ -0,0 +1,25 @@
+@echo off
+chcp 65001 >nul
+echo  启动中，请耐心等待 
+REM 定义 Anaconda 路径
+set CONDA_PATH=D:\ProgramData\anaconda3
+
+REM 激活目标虚拟环境
+CALL "%CONDA_PATH%\condabin\conda.bat" activate "D:\AI\SenseVoice\venv"
+
+REM 检查是否激活成功
+IF ERRORLEVEL 1 (
+    echo 激活虚拟环境失败，请检查路径或环境名称！
+    pause
+    exit /b
+)
+
+REM 设置 GPU 环境变量，选择显卡
+set CUDA_VISIBLE_DEVICES=1
+SET SENSEVOICE_DEVICE=cuda:1
+
+REM 执行 Python 脚本
+python api.py
+
+REM 保持窗口打开
+pause
